ed4f3bf3a24c42b318fdfa01e5a4b9e649ea3da0
hxgdsjzuyt1234hg <jaihacku@gmail.com>
2025-03-25 07:53:11 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+const global = {
+    ownerId: [7865363008],
+    botName: "XJUNZ-PRIV-BOT",
+    botToken: "7585283331:AAFBTZMSJrBLhAvEYSTkS5OX93vFchNoHqw",
+    channelLink: "https://t.me/NullxTeam",
+    preview: 'https://files.catbox.moe/i4185s.mp4'
+}
+
+module.exports = global;
